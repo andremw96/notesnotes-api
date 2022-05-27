c2670f2d2c2d75a80600db57c0929962d5c5fa0d
andremw96 <andre19961@gmail.com>
2022-05-27 14:11:40 +0700

    add sqlc crud

---
+++ db/query/users.sql
@@ -0,0 +1,26 @@
+-- name: CreateUsers :one
+INSERT INTO users (
+  full_name, first_name, last_name, username, email, password
+) VALUES (
+  $1, $2, $3, $4, $5, $6
+) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE users 
+SET first_name = $2, last_name = $3, full_name = $4, email = $5, password = $6
+WHERE id = $1 
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"andre/notesnotes-api/handler"
-	"andre/notesnotes-api/model"
-
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
 func main() {
-	//db.InitializeDb()
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&model.Book{})
-
-	handler := handler.NewHandler(db)
-
-	r := gin.New()
-
-	r.GET("/books", handler.ListBooksHandler)
-	r.POST("/books", handler.CreateBookHandler)
-	r.DELETE("/books/:id", handler.RemoveBookHandler)
-
-	r.Run()
 }
